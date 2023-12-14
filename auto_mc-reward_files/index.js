1c01579cf83bb82f2de3133c03a38c91b6dd41e6
yangxue <yangxue0827@126.com>
2023-12-14 19:17:31 +0800

    add auto_mc-reward project

---
+++ auto_mc-reward_files/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+//		bulmaCarousel.attach('#carousel-demo', {
+//			slidesToScroll: 1,
+//			slidesToShow: 3,
+//			loop: true,
+//		});
+    var options = {
+            initialSlide: 2,
+			slidesToScroll: 1,
+			slidesToShow: 2,
+			loop: true,
+			//infinite: true,
+            autoplay: true,
+            autoplaySpeed: 10000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+    	// bulmaCarousel instance is available as element.bulmaCarousel
+    	element.bulmaCarousel.on('before-show', function(state) {
+    		console.log(state);
+    	});
+    }
+})
