cc49463fd12d6878f62115091f7217816ed96092
Xue Yang <yangxue0827@126.com>
2026-07-31 21:31:30 +0800

    Update proxy usage in main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -7,7 +7,7 @@ from scholarly._proxy_generator import MaxTriesExceededException
 # Setup proxy
 pg = ProxyGenerator()
 pg.FreeProxies()  # Use free rotating proxies
-scholarly.use_proxy(pg)
+scholarly.use_proxy(pg, pg)
 
 
 try:
